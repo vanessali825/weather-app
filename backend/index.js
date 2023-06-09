32e6c3449e28b2f723de870162205616b06211d4
Vanessa Li <vanessali33@gmail.com>
2023-06-09 13:08:57 +0900

    second draft - main weather portion speed fixed

--- backend/index.js
+++ backend/index.js
@@ -16,29 +16,31 @@ app.use(bodyParser.json()) // parse application/json
 let lng;
 let lat;
 
+// GET REQUEST - INDEX PAGE
+app.get('/', async (req, res) => {
+    res.send('Hello world!')
+})
+
 // POST REQUEST - RECEIVE COORDINATE DATA FROM CLIENT 
 app.post('/coordinates', async (req, res) => {
     const body = await req.body;
     // console.log(body);    
     lat = body.Lat;
     lng = body.Lng;
-    console.log(lat, lng)
+    console.log(lat, lng);
+    res.redirect('/'); 
 })
 
 // GET REQUEST - REQUESTING DATA BASED ON COORDINATES
-app.get('/', async (req, res) => {
-    if (lng || lat != '') {
-        axios.get(`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lng}&current_weather=true&daily=temperature_2m_max,temperature_2m_min&timezone=auto`)
-            .then(response => {
-                res.send(response.data);
-                console.log(response.data)
-            })
-            .catch(error => {
-                console.log(error)
+app.get('/general', async (req, res) => {
+    await axios.get(`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lng}&current_weather=true&daily=temperature_2m_max,temperature_2m_min&timezone=auto`)
+        .then(response => {
+            res.send(response.data);
+            console.log(response.data)
         })
-    } else {
-        res.send('Hello world!')
-    }
+        .catch(error => {
+            console.log(error)
+    })
 })
 
 // SERVER
--- frontend/src/components/SearchBar.js
+++ frontend/src/components/SearchBar.js
@@ -17,6 +17,9 @@ export default function SearchBar(){
         Lng: ''
     })
 
+    // Capture count state
+    const [count, setCount] = useState(0)
+    
     // Capture weather data based on specified coordinates
     const [data, setData] = useState({
         Temperature: '',
@@ -36,39 +39,45 @@ export default function SearchBar(){
 
     // Effect when location state is updated; pull lat + lng coordinate information
     useEffect(() => {
-        axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${location.String}&key=${process.env.REACT_APP_API_KEY}`) 
-            .then(response => {
-                let data = response.data.results[0].geometry.location;
-                setCoordinates({
-                    Lat: data.lat,
-                    Lng: data.lng
+        if (location.String !== '') { // condition prevents this useEffect from mounting before actual call
+            axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${location.String}&key=${process.env.REACT_APP_API_KEY}`) 
+                .then(response => {
+                    let data = response.data.results[0].geometry.location;
+                    setCoordinates({
+                        Lat: data.lat,
+                        Lng: data.lng
+                    })
                 })
-            })
-            .catch(error => {console.log(error) })   
-        },[location])
+                .catch(error => {console.log(error)})   
+        }
+    },[location])
     
     // Effect when coordinates state is updated; post coordinate info to server
     useEffect(() => {
-        axios.post(`${process.env.REACT_APP_URL}/coordinates`, coordinates)
-            // .then(() => console.log(coordinates, 'Input delivered'))
-            .catch((error) => console.log(error))
-    }, [coordinates]);
+        if (coordinates.Lat !== '') { // condition prevents this useEffect from mounting before actual call
+            axios.post(`${process.env.REACT_APP_URL}/coordinates`, coordinates)
+                .then(() => console.log(coordinates, 'Input delivered'))
+                .then(() => setCount(count + 1))
+                .catch((error) => console.log(error))
+        }}
+    , [coordinates]);
 
     // Get weather info from server & extract relevant data
     useEffect(() => {
-        axios.get(`${process.env.REACT_APP_URL}`)
-            .then(response => {
-                let data = response.data.current_weather;
-                let weatherText = weatherCodes[data.weathercode];
-                console.log(data.temperature, weatherText, data.windspeed)
-                setData({
-                    Temperature: data.temperature,
-                    Weathercode: weatherText, 
-                    Windspeed: data.windspeed,
-                })
-            })     
-            .catch(error => console.error(error))
-    }, [data]);
+        if (count !== 0) { // condition prevents this useEffect from mounting before actual call
+          axios.get(`${process.env.REACT_APP_URL}/general`)
+                .then(response => {
+                    let data = response.data.current_weather;
+                    let weatherText = weatherCodes[data.weathercode];
+                    console.log(data.temperature, weatherText, data.windspeed)
+                    setData({
+                        Temperature: data.temperature,
+                        Weathercode: weatherText, 
+                        Windspeed: data.windspeed,
+                    })
+                })     
+                .catch(error => console.error(error))
+            }}, [count]);
 
     return(
         <div>
@@ -81,7 +90,7 @@ export default function SearchBar(){
                 />
                 <button type="submit">Search</button>
             </form>
-            {data.Temperature != '' ? 
+            {data.Temperature !== '' ? 
                 <WeatherInfo 
                     temperature={data.Temperature}
                     weathercode={data.Weathercode}
